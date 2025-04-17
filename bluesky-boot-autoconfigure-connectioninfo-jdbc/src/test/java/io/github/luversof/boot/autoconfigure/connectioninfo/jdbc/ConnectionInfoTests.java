36907bd39ac6e2f1e378b270ffa8dd55a43f867a
luversof <luversof@hanmail.net>
2025-04-18 02:38:13 +0900

    Disable `ConnectionInfoTests` and remove unused datasource modules.
    - Annotated `ConnectionInfoTests` with `@Disabled`.
    - Removed `bluesky-boot-connectioninfo-datasource` module from
    `pom.xml`.
    - Removed `bluesky-boot-autoconfigure-connectioninfo-datasource` module.
    - Updated `pom.xml` to reflect module changes.

--- bluesky-boot-autoconfigure-connectioninfo-jdbc/src/test/java/io/github/luversof/boot/autoconfigure/connectioninfo/jdbc/ConnectionInfoTests.java
+++ bluesky-boot-autoconfigure-connectioninfo-jdbc/src/test/java/io/github/luversof/boot/autoconfigure/connectioninfo/jdbc/ConnectionInfoTests.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -23,6 +24,7 @@ import io.github.luversof.boot.security.crypto.env.DecryptEnvironmentPostProcess
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Disabled
 @ActiveProfiles("localDev")
 class ConnectionInfoTests {
 
